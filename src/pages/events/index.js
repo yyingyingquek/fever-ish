e3f8f5da0b33781d055e34ebb6b856769e57baa1
Ying Ying <quekyingying26@gmail.com>
2023-04-06 11:21:30 +0800

    events-search component

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,8 +1,28 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import EventList from '@/components/events/event-list';
+import EventsSearch from '@/components/events/events-search';
+import { getAllEvents } from 'dummy-data';
+
 function AllEventsPage() {
+  const router = useRouter();
+  const allEvents = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    // programmatically navigate to slug page
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All Events Page</h1>
-    </div>
+    <Fragment>
+      {/* pass the function into EventsSearch component, no execution here.
+        Execute it in EventsSeach component after the form is submitted
+      */}
+      <EventsSearch onSearch={findEventsHandler}></EventsSearch>
+      <EventList items={allEvents}></EventList>
+    </Fragment>
   );
 }
 
